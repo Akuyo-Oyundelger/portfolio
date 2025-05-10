10e21392ab00ea2b89126fe802a5e8a9b121c2f9
Akuyo-Oyundelger <akuyo@mailbox.unideb.hu>
2025-05-10 22:38:48 +0200

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+// Smooth scroll (optional enhancement)
+document.querySelectorAll('nav a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
